de6d81b4919b699c314acb9c2858fccb19e94580
Matt McNabb <matt.mcnabb@allsop.software>
2018-01-12 14:50:54 +0000

    Added Table Module file

---
+++ src/table.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+
+import { TableComponent } from './table.component';
+import { ColumnDataComponent } from './column-data/column-data.component';
+import { ColumnFilterComponent } from './column-filter/column-filter.component';
+import { ColumnHeaderComponent } from './column-header/column-header.component';
+
+@NgModule({
+  declarations: [
+    TableComponent,
+    ColumnDataComponent,
+    ColumnFilterComponent,
+    ColumnHeaderComponent
+  ],
+  exports: [
+    TableComponent,
+    ColumnDataComponent,
+    ColumnFilterComponent,
+    ColumnHeaderComponent
+  ]
+})
+
+export class AllsopTableModule { }
